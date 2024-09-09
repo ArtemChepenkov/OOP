30e73daa3fadb5fc0bec4b052dd0b151020c91a7
Artem Chepenkov <a.chepenkov@g.nsu.ru>
2024-09-09 22:55:49 +0700

    gitignore

--- Task_1_1_1/build.sh
+++
@@ -1,5 +0,0 @@
-javadoc -d build/docs/javadoс -sourcepath src/main/java -subpackages ru.nsu.chepenkov
-
-javac src/main/java/ru/nsu/chepenkov/HeapSort.java -d ./build
-
-java -cp ./build ru.nsu.chepenkov.HeapSort
