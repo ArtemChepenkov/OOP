6253913c71096d30c59b543316bc800c63e6f591
Artem Chepenkov <a.chepenkov@g.nsu.ru>
2024-09-18 00:36:24 +0700

    added tests CardDeck

---
+++ Task_1_1_2/src/test/java/ru/nsu/chepenkov/CardDeckTest.java
@@ -0,0 +1,42 @@
+package ru.nsu.chepenkov;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class CardDeckTest {
+
+    @Test
+    @DisplayName("cardDeckTestClassConstructor")
+    void cardDeckTestClassConstructor() {
+        final int deckAmount = 2;
+        int counter = 0;
+        CardDeck cardDeck = new CardDeck(deckAmount);
+        Card[] testCards = {new Card("Червы", "Тройка", 3),
+                new Card("Буби", "Король", 10),
+                new Card("Пики", "Девятка", 9),
+                new Card("Трефы", "Туз", 11)};
+        for (int i = 0; i < testCards.length; i++) {
+            for (int j = 0; j < 52 * deckAmount; j++) {
+                if (testCards[i].getNumber() == cardDeck.cards[j].getNumber() &&
+                        testCards[i].getName().equals(cardDeck.cards[j].getName()) &&
+                        testCards[i].getSuit().equals(cardDeck.cards[j].getSuit())) {
+                    counter++;
+                }
+            }
+        }
+        assertEquals(counter, testCards.length * deckAmount);
+        assertNotNull(cardDeck);
+    }
+
+    @Test
+    @DisplayName("cardDeckTestTakeLastCard")
+    void cardDeckTestTakeLastCard() {
+        final int deckAmount = 2;
+        CardDeck cardDeck = new CardDeck(deckAmount);
+        Card card;
+        card = cardDeck.takeLastCard();
+        assertNotNull(card);
+    }
+}
--- Task_1_1_2/src/test/java/ru/nsu/chepenkov/CardTest.java
+++ Task_1_1_2/src/test/java/ru/nsu/chepenkov/CardTest.java
@@ -3,7 +3,6 @@ package ru.nsu.chepenkov;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import ru.nsu.chepenkov.Card;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
