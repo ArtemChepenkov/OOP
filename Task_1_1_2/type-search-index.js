029b94b7bbfe6e96ea050e2d34cae7ab8759dca1
ArtemChepenkov <ArtemChepenkov@users.noreply.github.com>
2024-09-17 16:13:44 +0000

    Deploying to gh-pages from @ ArtemChepenkov/OOP@450e372549f1f90cf3d6bfe96adaaa7eaf585657 🚀

---
+++ Task_1_1_2/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.chepenkov","l":"Card"},{"p":"ru.nsu.chepenkov","l":"CardDeck"},{"p":"ru.nsu.chepenkov","l":"Main"},{"p":"ru.nsu.chepenkov","l":"Table"}]
