fd3cddf2ca1572a32b1cb5257f1ad5154a53f2ab
Artem Chepenkov <a.chepenkov@g.nsu.ru>
2024-10-29 13:53:06 +0700

    added lots of things

---
+++ Task_1_2_2/src/test/java/ru/nsu/chepenkov/hashtable/HashTableTest.java
@@ -0,0 +1,81 @@
+package ru.nsu.chepenkov.hashtable;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class HashTableTest {
+
+    @Test
+    @DisplayName("HashTableTestOneElementTest")
+    void hashTableTestOneElementTest() {
+        HashTable<String, Number> hashTable = new HashTable<>();
+        hashTable.put("one", 1);
+        assertEquals(1, hashTable.get("one"));
+    }
+
+    @Test
+    @DisplayName("HashTableTestEqualKeys")
+    void hashTableTestEqualKeys() {
+        HashTable<String, Number> hashTable = new HashTable<>();
+        hashTable.put("one", 1);
+        hashTable.put("one", 1);
+        hashTable.put("one", 1);
+        hashTable.put("one", 1);
+        hashTable.put("one", 1);
+
+        assertEquals(1, hashTable.get("one"));
+        assertEquals(1, hashTable.size());
+    }
+
+    @Test
+    @DisplayName("HashTableTestRemove")
+    void hashTableTestRemove() {
+        HashTable<String, Number> hashTable = new HashTable<>();
+        hashTable.put("one", 1);
+        hashTable.put("two", 2);
+        hashTable.put("three", 3);
+        hashTable.remove("two");
+        hashTable.remove("three");
+
+        assertNull(hashTable.get("two"));
+        assertEquals(1, hashTable.size());
+    }
+
+    @Test
+    @DisplayName("HashTableTestUpdate")
+    void hashTableTestUpdate() {
+        HashTable<String, Number> hashTable = new HashTable<>();
+        hashTable.put("one", 1);
+        hashTable.update("one", 2);
+        assertEquals(2, hashTable.get("one"));
+    }
+
+    @Test
+    @DisplayName("HashTableTestBigSize")
+    void HashTableTestBigSize() {
+        HashTable<Number, Number> hashTable = new HashTable<>();
+        for (int i = 0; i < 100000; i++) {
+            hashTable.put(i, i);
+        }
+        assertEquals(55555, hashTable.get(55555));
+    }
+
+    @Test
+    @DisplayName("HashTableTestEquals")
+    void hashTableTestEquals() {
+        HashTable<Number, Number> hashTable1 = new HashTable<>();
+        hashTable1.put(1, 1);
+        hashTable1.put(2, 2);
+        hashTable1.put(3, 3);
+        HashTable<Number, Number> hashTable2 = new HashTable<>();
+        hashTable2.put(1, 1);
+        hashTable2.put(2, 2);
+        assertNotEquals(hashTable1, hashTable2);
+        hashTable1.remove(3);
+        assertEquals(hashTable1, hashTable2);
+    }
+
+    
+}
