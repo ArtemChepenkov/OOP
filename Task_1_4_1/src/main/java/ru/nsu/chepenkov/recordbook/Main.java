6f3badf7d80be294ff8e350fe94927fe1e6dca12
Artem Chepenkov <a.chepenkov@g.nsu.ru>
2024-12-03 13:27:36 +0700

    Remove Main class

--- Task_1_4_1/src/main/java/ru/nsu/chepenkov/recordbook/Main.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.chepenkov.recordbook;
-
-public class Main {
-    public static void main(String[] args) {
-        // Создаём экземпляр Semestr с необходимыми оценками
-        Semestr semestr = new Semestr(5, 4, 3, 4, 2, 5, 5, 4);
-        //System.out.println(semestr.isPossibleBudgetTransfer());
-    }
-}
